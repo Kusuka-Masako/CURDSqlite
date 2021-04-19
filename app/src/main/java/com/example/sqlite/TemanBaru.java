4f68d3c0704660293994336d537350b0b6cad76b
Kusuka-Masako <79459969+Kusuka-Masako@users.noreply.github.com>
2021-04-19 21:32:57 +0700

    ACT6

---
+++ app/src/main/java/com/example/sqlite/TemanBaru.java
@@ -0,0 +1,56 @@
+package com.example.sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.sqlite.database.DBcontroller;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.HashMap;
+
+public class TemanBaru extends AppCompatActivity {
+    private TextInputEditText tNama, tTelpon;
+    private Button savebtn;
+    String nm,tlp;
+    DBcontroller controller = new DBcontroller(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teman_baru);
+
+        tNama = (TextInputEditText)findViewById(R.id.tietNama);
+        tTelpon = (TextInputEditText)findViewById(R.id.tietTelpon);
+        savebtn = (Button)findViewById(R.id.buttonSave);
+
+        savebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tNama.getText().toString().isEmpty() || tTelpon.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Data belum Komplit !", Toast.LENGTH_SHORT).show();
+                }else {
+                    nm = tNama.getText().toString();
+                    tlp = tTelpon.getText().toString();
+
+                    HashMap<String,String> qvalues = new HashMap<>();
+                    qvalues.put("nama",nm);
+                    qvalues.put("telpon",tlp);
+
+                    controller.insertData(qvalues);
+                    callHome();
+                }
+            }
+        });
+    }
+
+    public void callHome(){
+        Intent intent = new Intent(TemanBaru.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
